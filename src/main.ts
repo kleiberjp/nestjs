f7bbd68c66b1b009dfecd1e3a7d542b61bbb00f5
Kleiber J Perez <kleiberjp@gmail.com>
2023-05-17 10:39:32 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { AppModule } from '@/app.module';
-import { HttpExceptionFilter } from '@/exception-filter';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
